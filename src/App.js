0a3a5236e60a35c9ae4335065bab5b35207432e7
Lissy Duncan <duncanlissy@gmail.com>
2024-11-12 21:12:53 +1100

    first react app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+const foods = ['pizza', 'pasta', 'spaghetti', 'ramen', 'eggs bennedict', 'dumplings', 'noodles', 'pork bun'];
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Our Menu!</h1>
+      <ul>
+        {
+          foods.map(food => {
+            return <li>{food}</li>
+          })
+        }
+      </ul>
+    </div>
+  )
+}
+
+
+export default App;
